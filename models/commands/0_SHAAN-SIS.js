dae8f0406b8d3968a09b2273133000efd1645d9f
Ahmad030687 <ahmadalisafdar86@gmail.com>
2026-03-13 16:47:21 +0500

    Update 0_SHAAN-SIS.js

--- models/commands/0_SHAAN-SIS.js
+++ models/commands/0_SHAAN-SIS.js
@@ -72,6 +72,6 @@ module.exports.run = async function ({ event, api, args }) {
     if (!mention[0]) return api.sendMessage("Please mention 1 person.", threadID, messageID);
     else {
         const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: "❥︎|===『  ♥️ SHAAN BABU ♥️  』===|☻︎", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
+        return makeImage({ one, two }).then(path => api.sendMessage({ body: "❥︎|===『  ♥️ 𝐀𝐇𝐌𝐀𝐃 𝐑𝐃𝐗 ♥️  』===|☻︎", attachment: fs.createReadStream(path) }, threadID, () => fs.unlinkSync(path), messageID));
     }
 }
